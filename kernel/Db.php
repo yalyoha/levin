921a5729d0e139e9108e415a33160c35558fc482
yalyoha <aleksey@eto-design.ru>
2016-09-10 22:43:55 +0300

    db

--- kernel/Db.php
+++ kernel/Db.php
@@ -1,20 +1,12 @@
 <?php
-
-require 'mysqlcong.php';
-
 class Db {
 
     static $conn = null;
 
     static function conn() {
-        if (isset(self::$conn))
-            return self::$conn;
-            
+        if (isset(self::$conn)) return self::$conn;
         self::$conn = new mysqli(SQL_HOST, SQL_USER, SQL_PASS, SQL_BASE);
-
-        if (self::$conn->connect_error)
-            die("Connection failed: " . self::$conn->connect_error);
-
+        if (self::$conn->connect_error) die("Connection failed: " . self::$conn->connect_error);
         return self::$conn;
     }
 
